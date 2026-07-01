7d93daba5339d6fe88dbcb6aa0c93a33f79ace71
Jhoann Chiles <jhoann.chiles@epn.edu.ec>
2026-07-01 10:41:30 -0500

    Ejercicio1 Realizado

--- arreglos.h
+++ arreglos.h
@@ -1,6 +1,18 @@
 #ifndef ARREGLOS_H
 #define ARREGLOS_H
 
+float media (float arreglo[], int n);
+float desviacion (float arreglo[], int n);
+float varianza (float arreglo[], int n);
+float curtosis (float arreglo[], int n);
+float maximo (float arreglo[], int n);
+float minimo (float arreglo[], int n);
+
+
+
+
+
+
 
 
 
