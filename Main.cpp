c7d9cdd310515a69cef37f12ff67d1a234ec44b0
MirkaEnriquez21 <mirka.enriquez@epn.edu.ec>
2026-07-01 11:00:34 -0500

    Poner en el main el  ejercicio3

--- Main.cpp
+++ Main.cpp
@@ -39,7 +39,21 @@ int main(){
 	transpuesta(matriz);   
 	cout << "-Traza de la matriz: " << traza(matriz) << endl; 
 	simetria(matriz);
-	
+
+
+	//--------Ejercicio 3--------
+    char frase[] = "El primer hombre en pisar la luna fue Neil Amstrong";
+    char siglas[100];
+    obtenerSiglas(frase, siglas);
+    cout << "Las siglas de la cadena son: " << siglas << endl;
+
+    char texto[] = "12345";
+    cout << "De string a entero: " << stringAEntero(texto) << endl;
+
+    char frase2[] = "la luna fue Neil";
+    cout << "El caracter mas repetido es : " << caracterMasRepetido(frase2) << endl;
+    
+    return 0;
 =======
     
 	
